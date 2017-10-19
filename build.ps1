0e0bb9974d76443b080fc7e9d6745097fe33fc63
Tobias Fenster <tobias.fenster@axians-infoma.de>
2017-10-19 22:10:06 +0200

    more debug

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
 mkdir c:\result
 $command = "c:\projects\$env:APPVEYOR_PROJECT_NAME\compile.bat"
 Write-Output "running docker run -v c:\result:c:\result arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build $command"
+dir c:\projects
+dir c:\projects\navonappveyor
 docker run -v c:\result:c:\result arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build $command
