667220b4d06cc6da1c975bac7b7ddddc1a782a1b
Tobias Fenster <tobias.fenster@axians-infoma.de>
2017-10-19 21:59:25 +0200

    debug

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 mkdir c:\result
+Write-Output "running docker run -v c:\result:c:\result arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build c:\projects\$env:APPVEYOR_PROJECT_NAME\compile.bat"
 docker run -v c:\result:c:\result arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build c:\projects\$env:APPVEYOR_PROJECT_NAME\compile.bat
