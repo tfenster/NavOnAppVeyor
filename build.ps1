dbc4bbe71f19390b9e53f02e673635d091ddc674
Tobias Fenster <tobias.fenster@axians-infoma.de>
2017-10-19 22:29:27 +0200

    map project into container

--- build.ps1
+++ build.ps1
@@ -1,6 +1,4 @@
 mkdir c:\result
 $command = "c:\projects\$env:APPVEYOR_PROJECT_NAME\compile.bat"
-Write-Output "running docker run -v c:\result:c:\result arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build $command"
-dir c:\projects
-dir c:\projects\navonappveyor
-docker run -v c:\result:c:\result arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build $command
+Write-Output "running docker NAV build image"
+docker run -v c:\result:c:\result -v c:\projects:c\projects arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build $command
