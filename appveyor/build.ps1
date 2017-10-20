4b05e4ea2295d503d617aa753f9f2b4229961d0d
Tobias Fenster <tobias.fenster@axians-infoma.de>
2017-10-20 12:55:32 +0200

    Suppress output of dir create

--- appveyor/build.ps1
+++ appveyor/build.ps1
@@ -1,4 +1,4 @@
-mkdir c:\result
+mkdir c:\result | Out-Null
 $command = "c:\projects\$env:APPVEYOR_PROJECT_NAME\appveyor\compile.bat"
 Write-Output "run docker NAV build image"
 docker run -v c:\result:c:\result -v c:\projects:c:\projects -e APPVEYOR_PROJECT_NAME=$env:APPVEYOR_PROJECT_NAME arssolvendi.azurecr.io/dynamics-nav:devpreview-finus-build $command
